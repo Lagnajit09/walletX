19f0ad2e13567e230039dba5c793c2a616ef79f9
Lm09 <123.moharana@gmail.com>
2024-08-08 22:34:00 +0530

    add: authOptions, sigin route

--- apps/user-app/next.config.mjs
+++ apps/user-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.mjs in apps/user-app
+import dotenv from "dotenv";
 
+dotenv.config({ path: "../../packages/db/.env" });
 export default nextConfig;
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
+
+export default db;
 export * from "@prisma/client";
