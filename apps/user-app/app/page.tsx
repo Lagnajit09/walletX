d9c0b236dfda2c9de287b9833b05882c6a909b40
Lm09 <123.moharana@gmail.com>
2024-08-09 22:33:20 +0530

    add: pages

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
-
 export default function Page(): JSX.Element {
-  const session = useSession();
-  console.log(session.data);
-  return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-    </div>
-  );
+  return <></>;
 }
