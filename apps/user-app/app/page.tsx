ee03f7630a8b1d93d5ec0691368d5db7fff574bc
Lm09 <123.moharana@gmail.com>
2024-08-09 23:18:16 +0530

    update: root page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Page(): JSX.Element {
-  return <></>;
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/api/auth/signin");
+  }
 }
