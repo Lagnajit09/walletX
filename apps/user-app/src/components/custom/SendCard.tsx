079818c37a44a76a3a9d729ac0f1c2db13e3f341
Lm09 <123.moharana@gmail.com>
2024-10-20 13:52:24 +0530

    add: p2p transfer

--- apps/user-app/src/components/custom/SendCard.tsx
+++ apps/user-app/src/components/custom/SendCard.tsx
@@ -1,14 +1,54 @@
 "use client";
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
-import { Center } from "@repo/ui/center";
 import { TextInput } from "@repo/ui/textinput";
 import { useState } from "react";
 import { p2pTransfer } from "../../../app/lib/actions/p2pTransfer";
+import { useSession } from "next-auth/react";
+import ErrorModal from "./ErrorModal";
+import { CheckPin } from "./CheckPin";
 
 export function SendCard() {
   const [number, setNumber] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [errMsg, setErrMsg] = useState({
+    title: "",
+    description: "",
+  });
+  const [showError, setShowError] = useState(false);
+  const [showPinModal, setShowPinModal] = useState(false);
+  const session = useSession();
+
+  const handleTriggerPinModal = () => {
+    setShowPinModal(!showPinModal);
+  };
+
+  const transferMoneyHandler = async (pinIsValid: boolean) => {
+    if (!pinIsValid) {
+      setErrMsg({
+        title: "Incorrect Pin",
+        description: "Please provide the correct pin.",
+      });
+      setShowError(true);
+      return;
+    }
+    try {
+      await p2pTransfer(number, amount * 100);
+      setErrMsg({
+        title: "Success",
+        description: `${amount} INR sent to ${number} successfully!`,
+      });
+      setShowError(true);
+    } catch (error) {
+      setErrMsg({
+        title: "Failed",
+        description: "Failed to send money.",
+      });
+      setShowError(true);
+    } finally {
+      window.location.reload();
+    }
+  };
 
   return (
     <div className="w-[40%] mt-10">
@@ -17,6 +57,25 @@ export function SendCard() {
         classname="bg-[#023e8a] rounded-lg text-gray-300"
         titleClass="text-white font-semibold"
       >
+        <ErrorModal
+          title={errMsg.title}
+          description={errMsg.description}
+          continueBtn="Okay"
+          open={showError}
+          setOpen={setShowError}
+        />
+        <ErrorModal
+          title={errMsg.title}
+          description={errMsg.description}
+          continueBtn="Okay"
+          open={showError}
+          setOpen={setShowError}
+        />
+        <CheckPin
+          open={showPinModal}
+          setOpen={handleTriggerPinModal}
+          addMoneyHandler={transferMoneyHandler}
+        />
         <div className="min-w-72 pt-2">
           <TextInput
             placeholder={"Number"}
@@ -30,19 +89,29 @@ export function SendCard() {
             placeholder={"Amount"}
             label="Amount"
             onChange={(value) => {
-              setAmount(value);
+              setAmount(Number(value));
             }}
             labelClass="text-gray-200"
           />
           <div className="pt-4 flex justify-center">
             <Button
+              disable={amount === 0 || number === "" ? true : false}
+              classname="bg-white text-black font-semibold hover:bg-gray-200"
               onClick={async () => {
-                await p2pTransfer(number, Number(amount) * 100);
+                if (session?.data?.user?.pin) {
+                  handleTriggerPinModal();
+                } else {
+                  setErrMsg({
+                    title: "Please Set Your Pin First!",
+                    description:
+                      "To update your pin, Go to Profile > Settings > Pin",
+                  });
+                  setShowError(true);
+                }
               }}
-              classname="bg-gray-100 text-gray-800 font-semibold hover:bg-gray-800 hover:text-gray-100"
             >
               Send
-            </Button>{" "}
+            </Button>
           </div>
         </div>
       </Card>
