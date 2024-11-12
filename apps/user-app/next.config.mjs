45677bea0a8c6427e02422f4bde6324116b88800
Lm09 <123.moharana@gmail.com>
2024-11-12 14:40:47 +0530

    Clean main branch

--- apps/user-app/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-// next.config.mjs in apps/user-app
-import dotenv from "dotenv";
-
-dotenv.config({ path: "../../packages/db/.env" });
-export default nextConfig;
--- packages/db/prisma/migrations/20241020111547_p2p_transfer_index/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "p2pTransfer_fromUserId_idx" ON "p2pTransfer"("fromUserId");
-
--- CreateIndex
-CREATE INDEX "p2pTransfer_toUserId_idx" ON "p2pTransfer"("toUserId");
-
--- CreateIndex
-CREATE INDEX "p2pTransfer_fromUserId_toUserId_idx" ON "p2pTransfer"("fromUserId", "toUserId");
